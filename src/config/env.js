6954716b47f3008e855fb29487d3ac4e0780456f
agfayuniar <hagi.agfayuniar@prosia.co.id>
2026-01-14 21:02:48 +0700

    chore: remove unused variable in contact section and env

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,3 @@
-export const myEmail = import.meta.env.VITE_MY_EMAIL
-export const myPhone = import.meta.env.VITE_MY_PHONE
-export const myLocation = import.meta.env.VITE_MY_LOCATION
-
 export const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID
 export const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID
 export const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY
